13d440337e5f838c8a5704a4d0962a999a59868e
Max Lee <maxbmhlee@gmail.com>
2023-03-29 00:34:02 -0400

    created shell of rebuilt site

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,33 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			smartypants: {
+				quotes: true,
+				ellipses: true,
+				backticks: false,
+				dashes: 'inverted'
+			},
+			rehypePlugins: [rehypeSlug]
+		})
+	],
 	kit: {
 		adapter: adapter({
 			pages: 'dist'
-		})
+		}),
+		alias: {
+			'@site': './src'
+		}
 	}
 };
 
