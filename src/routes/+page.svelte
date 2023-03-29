f2338a181f458227c1ba00b35c49a8a9329f73a6
Max Lee <maxbmhlee@gmail.com>
2023-03-29 00:57:13 -0400

    fix linting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import Example from "@site/routes/Example.svelte";
+	import Example from '@site/routes/Example.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<Example/>
+<Example />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
